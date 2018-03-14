9091125547d3f426690f0e64f760df6afa04e981
khvan-innokenty <khvan.innokenty@gmail.com>
2018-03-14 16:56:17 +0300

    Страница 404

---
+++ 404.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="" class="page404">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>404</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Fira+Sans:300,400,700&amp;subset=cyrillic" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css?v=1521034769">
+    <link rel="icon" type="image/png" href="/img/favicon.png" />
+</head>
+<body>
+    <img src="/img/404.svg" class="page404__icon">
+    <h2 class="page404__header">Страница не найдена</h2>
+    <a href="/" class="button button--secondary">На главную</a>
+    <div class="bottom-gap"></div>
+</body>
+</html>
